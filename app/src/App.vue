fe0ecbc1589536ee3c0c128550d3f93c44ec7698
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-10-16 22:37:52 -0500

    feat: Update App.vue and style.css
    - Update App.vue to use a universal header and footer components, and add a main content section with a router view.
    - Update style.css to apply a box-sizing border-box to all elements, optimize font legibility, and introduce a new color scheme.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-  </header>
-
-  <RouterView />
+  <div class="bg-gray-900 text-gray-100 flex flex-col min-h-full h-screen">
+    <!-- Universal Header -->
+    <UniversalHeader />
+    
+    <!-- Main Content -->
+    <main class="w-full flex-grow">
+      <router-view />
+    </main>
+    
+    <!-- Universal Footer -->
+    <UniversalFooter />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import UniversalHeader from '@/components/UniversalHeader.vue';
+import UniversalFooter from '@/components/UniversalFooter.vue';
+import { onBeforeMount } from 'vue';
+</script>

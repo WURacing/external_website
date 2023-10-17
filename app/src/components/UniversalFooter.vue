510c23fff5f7b972a6781d5763aa17d5415ef24e
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-10-16 22:39:31 -0500

    Add UniversalFooter component
    This commit adds the UniversalFooter component to the application. The component represents the footer section of the page and includes a RacingIcon. It is styled using CSS to have a background color, a border, and some padding.

---
+++ app/src/components/UniversalFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Footer -->
+  <footer class="mt-32 bg-gray-900 border-t border-gray-600" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
+      <!-- 
+        Logo
+      -->
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <RacingIcon />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import RacingIcon from '@/components/icons/RacingIcon.vue';
+</script>

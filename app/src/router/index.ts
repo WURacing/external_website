fb9c8a2f0c1202d8ca8bc4fce0f64b5bc27e9146
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-10-16 22:36:59 -0500

    feat: Add meta information for home and about routes
    - Added `meta` object for the `home` and `about` routes in the router configuration.
    - Updated the `component` property to include the imported views for the `home` and `about` routes.
    - Added `title` and `hideInFooter` properties to the `meta` object for both routes.
    - Added `beforeEach` navigation guard to set the document title based on `to.meta.title` before each route change.

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -7,17 +7,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home',
+        hideInFooter: false
+      }
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About',
+        hideInFooter: false
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next()
 })
 
 export default router

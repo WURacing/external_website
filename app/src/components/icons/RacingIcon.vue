82c247539290690de6b8d563800747534eeab78c
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-18 12:26:17 -0600

    Adjust size and filter of RacingIcon

--- app/src/components/icons/RacingIcon.vue
+++ app/src/components/icons/RacingIcon.vue
@@ -1,12 +1,12 @@
 <template>
-  <img src="../../assets/WashURacingRed.png" alt="Racing Icon" class="block h-16 w-auto" />
+  <img src="../../assets/WashURacingRed.png" alt="Racing Icon" class="block h-20 w-auto img" />
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
 /* Thicken the WashURacingRed.png lots */
-img {
-  filter: drop-shadow(0px 0px 10px rgba(255, 0, 0, 0.5));
+.img {
+  filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 1));
 }
 </style>

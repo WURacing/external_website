310dbfcb626b4e45760bd94ac5214813a440dd62
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-18 14:47:25 -0600

    feat(config): red now matches WURacing brand red

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -11,6 +11,21 @@ export default {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'red': {
+          '50': '#fef2f2',
+          '100': '#fde3e3',
+          '200': '#fccccc',
+          '300': '#f9a8a8',
+          '400': '#f37676',
+          '500': '#e94a4a',
+          '600': '#d63030',
+          '700': '#b32222',
+          '800': '#942020',
+          '900': '#7b2121',
+          '950': '#430c0c',
+        },
+      },
     },
   },
   plugins: [

671ee61d622af56f686bbac85e3cda4faf477341
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-18 14:08:53 -0600

    feat: Update UniversalHeader.vue component
    - Modify button class to include extra styles
    - Wrap transition properties onto multiple lines for better readability
    - Update class attributes to include additional styles
    - Refactor code to improve readability
    - Remove unused import statements
    - Remove about dropdown from navigation menu

--- app/src/components/UniversalHeader.vue
+++ app/src/components/UniversalHeader.vue
@@ -17,7 +17,8 @@
         </a>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-stone-400" @click="mobileMenuOpen = true">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-stone-400"
+          @click="mobileMenuOpen = true">
           <span class="sr-only">Open main menu</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
@@ -27,22 +28,28 @@
           Home
         </RouterLink>
         <Popover class="relative">
-          <PopoverButton class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-stone-100 hover:text-red-400 rounded-md focus-visible:outline-0 focus-visible:ring-0">
+          <PopoverButton
+            class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-stone-100 hover:text-red-400 rounded-md focus-visible:outline-0 focus-visible:ring-0">
             <span>Learn More</span>
             <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
           </PopoverButton>
 
-          <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+          <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1"
+            enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
+            leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
             <PopoverPanel class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-min -translate-x-1/2 px-4">
-              <div class="w-56 shrink rounded-xl bg-stone-100 p-4 text-sm font-semibold leading-6 text-stone-900 shadow-lg ring-1 ring-stone-900/5">
-                <RouterLink v-for="item in isDropdown(true).value" :key="item.name" :to="item.to" class="block p-2 hover:text-red-600">
+              <div
+                class="w-56 shrink rounded-xl bg-stone-100 p-4 text-sm font-semibold leading-6 text-stone-900 shadow-lg ring-1 ring-stone-900/5">
+                <RouterLink v-for="item in isDropdown(true).value" :key="item.name" :to="item.to"
+                  class="block p-2 hover:text-red-600">
                   {{ item.name }}
                 </RouterLink>
               </div>
             </PopoverPanel>
           </transition>
         </Popover>
-        <RouterLink v-for="item in isDropdown(false).value" :key="item.name" :to="item.to" class="text-sm font-semibold leading-6 text-stone-100 hover:text-red-400">
+        <RouterLink v-for="item in isDropdown(false).value" :key="item.name" :to="item.to"
+          class="text-sm font-semibold leading-6 text-stone-100 hover:text-red-400">
           {{ item.name }}
         </RouterLink>
       </div>
@@ -53,7 +60,8 @@
     -->
     <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
       <div class="fixed inset-0 z-50" />
-      <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-stone-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-stone-900/10">
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-stone-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-stone-900/10">
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">
@@ -69,8 +77,12 @@
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-stone-500/100">
             <div class="space-y-2 py-6">
-              <RouterLink to="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-stone-100 hover:bg-stone-50">Home</RouterLink>
-              <RouterLink v-for="item in navigation" :key="item.name" :to="item.to" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-stone-100 hover:bg-stone-50">{{ item.name }}</RouterLink>
+              <RouterLink to="/"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-stone-100 hover:bg-stone-50">
+                Home</RouterLink>
+              <RouterLink v-for="item in navigation" :key="item.name" :to="item.to"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-stone-100 hover:bg-stone-50">
+                {{ item.name }}</RouterLink>
             </div>
           </div>
         </div>
@@ -84,24 +96,19 @@ import RacingIcon from './icons/RacingIcon.vue';
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { ref } from 'vue'
-import { 
+import {
   Dialog, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel,
-  Popover, PopoverButton, PopoverPanel 
+  Popover, PopoverButton, PopoverPanel
 } from '@headlessui/vue'
 import { Bars3Icon, MinusSmallIcon, PlusSmallIcon, XMarkIcon, ChevronDownIcon } from '@heroicons/vue/24/outline'
 
 const route = useRoute();
-const current = computed(() =>route.path);
+const current = computed(() => route.path);
 const mobileMenuOpen = ref(false);
 
 const isDropdown = (isDropdown: boolean) => computed(() => navigation.filter(item => item.isDropdown === isDropdown));
 
 const navigation = [
-  {
-    name: 'About',
-    to: '/about',
-    isDropdown: true,
-  },
   {
     name: 'Gallery',
     to: '/gallery',

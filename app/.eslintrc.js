96620aea1c7694cbdb64b8e68288e13984f11ff6
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-18 16:32:59 -0600

    Refactor ESLint and Prettier configurations
    Update ESLint configuration to use recommended rules for Vue 3, AirBnb base, and Prettier. Update Prettier configuration to enforce single quotes, remove trailing commas, and use a two-space indent. Also remove unnecessary VSCode extensions from recommendations.
    
    This change improves code quality, ensures consistent coding style, and aligns with best practices for Vue 3 development. It simplifies the configuration files and eliminates redundant rules.

---
+++ app/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    indent: ['error', 2],
+  },
+}

916ea49777fd6c4b871e4b655aff69e196cade79
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-20 15:20:05 -0600

    Update UniversalFooter.vue with new bottom section

--- app/src/components/UniversalFooter.vue
+++ app/src/components/UniversalFooter.vue
@@ -5,12 +5,21 @@
     aria-labelledby="footer-heading"
   >
     <h2 id="footer-heading" class="sr-only">Footer</h2>
-    <div class="mx-auto py-10 sm:py-16 lg:py-20 px-8 sm:px-12 lg:px-16">
-      <!--
-        Logo
-      -->
-      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <RacingIcon />
+    <div class="mx-auto py-4 sm:py-8 lg:py-12 px-8 sm:px-12 lg:px-16" />
+
+    <!--
+      Bottom
+    -->
+    <div class="bg-stone-800">
+      <div class="mx-auto py-4 px-8 sm:px-12 lg:px-16">
+        <div class="flex justify-center space-x-6">
+          <!--
+            Legal
+          -->
+          <div class="flex items-center text-sm text-stone-300">
+            <span>&copy; {{ new Date().getFullYear() }}  WashU Racing - All Rights Reserved</span>
+          </div>
+        </div>
       </div>
     </div>
   </footer>

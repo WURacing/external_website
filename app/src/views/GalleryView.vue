49b18f8222a3ac87c160e9d688bc4ae61c25dd15
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-20 15:20:08 -0600

    Remove scroll event listener in GalleryView.vue

--- app/src/views/GalleryView.vue
+++ app/src/views/GalleryView.vue
@@ -1,24 +1,9 @@
 <template>
   <div
     class="relative min-h-fit isolate overflow-hidden border-t border-stone-600"
-    @scroll="handleScroll"
   />
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
 
-const scrollY = ref(0);
-
-const handleScroll = () => {
-  scrollY.value = window.scrollY;
-};
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll);
-});
 </script>

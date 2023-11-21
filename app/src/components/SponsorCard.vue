f8472e58f23a0252c7cc3c1306c17a4bbe227a1f
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-11-20 18:46:48 -0600

    Refactor SponsorCard and introduce TierBadge component
    This commit refactors the SponsorCard component by introducing the TierBadge component and making several visual improvements. The new TierBadge component dynamically applies gradient backgrounds based on the sponsor tier. The SponsorCard component now includes optional content, such as an image related to the sponsor. The AOS library is used for fade-up animations. These changes enhance the visual appeal of the sponsor cards and provide a more engaging user experience.

--- app/src/components/SponsorCard.vue
+++ app/src/components/SponsorCard.vue
@@ -1,33 +1,48 @@
 <template>
   <a
+    data-aos="fade-up"
     :href="sponsor.link"
+    class="sponsor-card block rounded-lg overflow-hidden shadow-lg transition-transform hover:shadow-2xl focus:outline-none"
     @mouseenter="animate"
     @mouseleave="resetAnimation"
     @focusin="animate"
-    @focusout="resetAnimation"
-    class="sponsor-card block rounded-lg overflow-hidden shadow-lg transition-transform hover:scale-105 hover:shadow-2xl">
-    <div class="flex justify-between items-center p-4 bg-gray-800">
-      <h3 class="font-bold text-lg text-white">{{ sponsor.name }}</h3>
-      <span
-        class="inline-block py-1 px-3 text-sm font-semibold rounded-full bg-gradient-to-r from-green-400 to-blue-500 text-white">{{
-        sponsor.tier }}</span>
+    @focusout="resetAnimation">
+    <div class="flex justify-between items-center p-4 bg-gradient-to-r from-gray-800 to-gray-900">
+      <div class="flex items-center">
+        <SponsorIcon :tier="sponsor.tier" class="h-8 w-8 text-white mr-2" />
+        <h3 class="font-bold text-lg text-white">{{ sponsor.name }}</h3>
+      </div>
+
+      <TierBadge :tier="sponsor.tier" />
+    </div>
+    <!-- Optional content -->
+    <div class="aspect-w-16 aspect-h-9 bg-gray-200">
+      <!-- Here could be a image or something related to sponsor -->
     </div>
   </a>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { gsap } from 'gsap';
-import {
-  Sponsor,
-  SponsorTier,
-} from '../types/sponsors.ts';
+import AOS from 'aos';
+import { Sponsor, SponsorTier } from '../types/sponsors';
+import TierBadge from './TierBadge.vue';
 
 defineProps<{ sponsor: Sponsor }>();
+
+onMounted(() => {
+  AOS.init({
+    duration: 800,
+    easing: 'ease-out-cubic',
+  });
+});
 
 const animate = (event: MouseEvent) => {
   const card = event.currentTarget as HTMLElement;
   gsap.to(card, {
-    scaleY: 1.05,
+    scale: 1.05,
+    borderRadius: '1rem',
     transformOrigin: 'bottom center',
     ease: 'power2.out',
   });
@@ -36,9 +51,14 @@ const animate = (event: MouseEvent) => {
 const resetAnimation = (event: MouseEvent) => {
   const card = event.currentTarget as HTMLElement;
   gsap.to(card, {
-    scaleY: 1,
+    scale: 1,
+    borderRadius: '0.5rem',
     transformOrigin: 'bottom center',
     ease: 'power2.out',
   });
 };
 </script>
+
+<style>
+/* Additional styles can be here */
+</style>
---
+++ app/src/components/TierBadge.vue
@@ -0,0 +1,29 @@
+<template>
+  <span class="inline-block py-1 px-3 text-sm font-semibold rounded-full text-white" :class="tierGradient">
+    {{ tier }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { SponsorTier } from '../types/sponsors.ts';
+
+const props = defineProps<{ tier: SponsorTier }>();
+
+const tierGradient = computed(() => {
+  switch (props.tier) {
+    case 'Diamond':
+      return 'bg-gradient-to-r from-pink-500 to-purple-600';
+    case 'Platinum':
+      return 'bg-gradient-to-r from-blue-400 to-indigo-500';
+    case 'Gold':
+      return 'bg-gradient-to-r from-yellow-400 to-orange-500';
+    case 'Silver':
+      return 'bg-gradient-to-r from-gray-400 to-gray-500';
+    case 'Bronze':
+      return 'bg-gradient-to-r from-orange-400 to-red-500';
+    default:
+      return '';
+  }
+});
+</script>

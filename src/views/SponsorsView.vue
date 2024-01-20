3fd98c66830e4594e53ac2cf5e64db599d6a9143
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2024-01-20 14:03:21 -0600

    Refactor SponsorsView.vue component and implement pagination

--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="relative min-h-screen isolate overflow-hidden flex flex-col items-center bg-stone-900 text-stone-100"
+  <div
+    class="relative min-h-screen isolate overflow-hidden flex flex-col items-center bg-stone-900 text-stone-100"
     :style="{ paddingTop: headerHeight + 'px' }">
     <div class="flex flex-col items-center w-full px-8 pb-8 max-w-7xl">
 
@@ -11,7 +12,9 @@
         <p class="text-lg sm:text-xl md:text-2xl text-stone-300">
           Our sponsors make our success possible.
         </p>
-        <button type="button" @click="openModal"
+        <button
+          type="button"
+          @click="openModal"
           class="mt-8 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-stone-100 bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400">
           <FontAwesomeIcon icon="file-pdf" class="mr-2" />
           Sponsorship Packet
@@ -19,8 +22,23 @@
       </div>
 
       <!-- Sponsor card grid -->
-      <div class="mt-12 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
-        <SponsorCard v-for="sponsor in sponsors" :key="sponsor.id" :sponsor="sponsor" />
+      <div class="mt-12 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+        <SponsorCard v-for="sponsor in sponsorsData.data" :key="sponsor.id" :sponsor="sponsor" class="sponsor-card" />
+      </div>
+
+      <!-- Pagination -->
+      <div class="mt-8 flex justify-center items-center space-x-4">
+        <button
+          @click="prevPage"
+          type="button"
+          :disabled="!sponsorsData.prev_page_url"
+          class="px-4 py-2 rounded-md bg-stone-800 text-stone-100 hover:bg-stone-700 disabled:opacity-50">Prev</button>
+        <p class="text-stone-100">Page {{ sponsorsData.current_page }} of {{ sponsorsData.last_page }}</p>
+        <button
+          @click="nextPage"
+          type="button"
+          :disabled="!sponsorsData.next_page_url"
+          class="px-4 py-2 rounded-md bg-stone-800 text-stone-100 hover:bg-stone-700 disabled:opacity-50">Next</button>
       </div>
 
       <!-- Modal for viewing sponsorship packet -->
@@ -39,8 +57,10 @@ import PDF from '../assets/sponsorship_packet2023.pdf';
 import SponsorCard from '../components/SponsorCard.vue';
 import SponsorshipModal from '../components/SponsorshipModal.vue';
 import {
-  Sponsor,
+  type Sponsor,
 } from '../types/sponsors.ts';
+import makeBackendAPIRequest from '../services/axios.ts';
+import { type Pagination } from '../types/pagination.ts';
 
 // Modal
 const isModalOpen = ref(false);
@@ -49,54 +69,70 @@ const isModalOpen = ref(false);
 const pdfSource = ref(PDF);
 
 // Sponsors
-const prevPage: Ref<String | null> = ref(null);
-const currentPage: Ref<number | null> = ref(null);
-const nextPage: Ref<String | null> = ref(null);
-const totalPages: Ref<number | null> = ref(null);
-const sponsors: Ref<Sponsor[]> = ref([]);
+const sponsorsData: Ref<Pagination<Sponsor>> = ref({
+  current_page: 1,
+  data: [],
+  first_page_url: '',
+  from: null,
+  last_page: 1,
+  last_page_url: '',
+  links: [],
+  next_page_url: null,
+  path: '',
+  per_page: 15,
+  prev_page_url: null,
+  to: null,
+  total: 0,
+});
 
-// Sample:
-// {
-//   "current_page": 1,
-//     "data": [
-//       {
-//         "id": 4,
-//         "name": "Boeing",
-//         "tier": "Diamond",
-//         "link": "https://www.boeing.com/",
-//         "image_path": "sponsors/boeing.jpg",
-//         "created_at": "2023-12-09T21:57:19.000000Z",
-//         "updated_at": "2023-12-09T22:00:22.000000Z"
-//       }
-//     ],
-//       "first_page_url": "http://wuracing-api.test/api/v1/sponsors?page=1",
-//         "from": 1,
-//           "last_page": 1,
-//             "last_page_url": "http://wuracing-api.test/api/v1/sponsors?page=1",
-//               "links": [
-//                 {
-//                   "url": null,
-//                   "label": "&laquo; Previous",
-//                   "active": false
-//                 },
-//                 {
-//                   "url": "http://wuracing-api.test/api/v1/sponsors?page=1",
-//                   "label": "1",
-//                   "active": true
-//                 },
-//                 {
-//                   "url": null,
-//                   "label": "Next &raquo;",
-//                   "active": false
-//                 }
-//               ],
-//                 "next_page_url": null,
-//                   "path": "http://wuracing-api.test/api/v1/sponsors",
-//                     "per_page": 15,
-//                       "prev_page_url": null,
-//                         "to": 1,
-//                           "total": 1
-// }
+const fetchSponsors = (url: string) => {
+  makeBackendAPIRequest<Pagination<Sponsor>>(url ?? '/sponsors')
+    .then((response) => {
+      // Map the response data to correctly match the Sponsor type
+      const sponsors = response.data.data.map((sponsor) => ({
+        id: sponsor.id,
+        name: sponsor.name,
+        tier: sponsor.tier,
+        link: sponsor.link,
+        imagePath: import.meta.env.VITE_APP_BACKEND_URL + sponsor.image_path,
+        createdAt: sponsor.created_at,
+        updatedAt: sponsor.updated_at,
+      }));
+
+      // Update the sponsorsData ref
+      sponsorsData.value = {
+        current_page: response.data.current_page,
+        data: sponsors,
+        first_page_url: response.data.first_page_url,
+        from: response.data.from,
+        last_page: response.data.last_page,
+        last_page_url: response.data.last_page_url,
+        links: response.data.links,
+        next_page_url: response.data.next_page_url,
+        path: response.data.path,
+        per_page: response.data.per_page,
+        prev_page_url: response.data.prev_page_url,
+        to: response.data.to,
+        total: response.data.total,
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+// Pagination methods
+const prevPage = () => {
+  if (sponsorsData.value.prev_page_url) {
+    fetchSponsors(sponsorsData.value.prev_page_url);
+  }
+};
+
+const nextPage = () => {
+  if (sponsorsData.value.next_page_url) {
+    fetchSponsors(sponsorsData.value.next_page_url);
+  }
+};
 
 // Methods for modal state management
 const openModal = () => (isModalOpen.value = true);
@@ -111,13 +147,8 @@ onMounted(() => {
     headerHeight.value = header.offsetHeight; // Get the outer height of the header
   }
 
-  this?.$axios.get('/sponsors').then((response) => {
-    sponsors.value = response.data.data;
-    prevPage.value = response.data.prev_page_url;
-    currentPage.value = response.data.current_page;
-    nextPage.value = response.data.next_page_url;
-    totalPages.value = response.data.last_page;
-  });
+  // Fetch sponsors
+  fetchSponsors('/sponsors');
 
   // Dynamically update page title
   const titles = document.querySelectorAll('[data-title]');
@@ -138,34 +169,6 @@ onMounted(() => {
   titles.forEach((title) => {
     // eslint-disable-next-line no-param-reassign
     title.innerHTML = title.innerHTML.toUpperCase();
-  });
-
-  // Select all sponsor cards
-  const cards = document.querySelectorAll('.sponsor-card');
-
-  // Set initial state of cards to invisible
-  gsap.set(cards, { autoAlpha: 0, y: 50 });
-
-  // Create a timeline for the stagger animation
-  const tl = gsap.timeline({
-    defaults: {
-      duration: 0.5,
-      ease: 'racingBounce',
-    },
-  });
-
-  tl.fromTo(cards, {
-    y: 50,
-    autoAlpha: 0,
-  }, {
-    autoAlpha: 1,
-    y: 0,
-    stagger: {
-      each: 0.1,
-      grid: 'auto',
-      from: 'edges',
-      amount: 1,
-    },
   });
 });
 </script>
